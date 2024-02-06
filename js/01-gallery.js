1a603c9db2fb46580966a0bb8407e4992ee5a29a
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-02-06 13:25:11 +0100

    goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.innerHTML = galleryMarkup;
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+// Dodajemy event listener do kontenera galerii, aby obsłużyć kliknięcia
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault(); // Zablokowanie domyślnego zachowania linka
+  const isGalleryImage = event.target.classList.contains("gallery__image");
+  if (!isGalleryImage) {
+    return;
+  }
+
+  const source = event.target.dataset.source;
+  openModalWithImage(source);
+}
+
+function openModalWithImage(src) {
+  const instance = basicLightbox.create(`
+    <img src="${src}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  // Dodanie obsługi zamknięcia modalu klawiszem Escape
+  window.addEventListener("keydown", onEscKeyPress);
+
+  function onEscKeyPress(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscKeyPress); // Usunięcie nasłuchiwania po zamknięciu
+    }
+  }
+}
