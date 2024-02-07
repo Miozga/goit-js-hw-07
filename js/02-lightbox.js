b6143f976d38680910d9b8bc67f6a9bcdbacfb97
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-02-07 15:47:44 +0100

    goit-js-hw-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `
+    )
+    .join("");
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+});
